aadcb489cf5e517b9b580619a72289f49a400f45
Aditya <75136463+Aditya121-bot@users.noreply.github.com>
2021-12-10 01:49:27 +0530

    Create File.php

---
+++ File.php
@@ -0,0 +1,8 @@
+<?php
+function password_gen($length = 8) {
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
+    $password = substr(str_shuffle($chars), 0, $length);
+    return $password;
+}
+echo password_gen();
+?>
